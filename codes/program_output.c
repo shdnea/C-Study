7027040bbcb9dc2354036d5c385fdc25930a8591
shdn <shdnea@gmail.com>
2020-11-20 23:12:25 +0700

    update with header file

--- codes/program_output.c
+++ codes/program_output.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 
-int main()
-{
+int main(){
     printf("from sea to shining C\n");
     return 0;
 }
---
+++ header/pacific_sea.h
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+#define AREA 2337
+#define SQ_MILES_PER_SQ_KILOMETER 0.386
+#define SQ_FEET_PER_SQ_MILE (5280 * 5280)
+#define SQ_INCHES_PER_SQ_FOOT 114
+#define ACRES_PER_SQ_MILE 640
